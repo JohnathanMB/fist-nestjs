3aaaa2b12c53b6b58a59b4b3bb96646f12f33536
Johnathan Arley Monsalve Bello <johnathan.monsalve@sofka.local>
2024-02-20 10:43:01 -0500

    home controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AppModule } from './app/app.module';
+import { HomeController } from './home/home.controller';
 
 @Module({
   imports: [AppModule],
-  controllers: [AppController],
+  controllers: [AppController, HomeController],
   providers: [AppService],
 })
 export class AppModule {}
